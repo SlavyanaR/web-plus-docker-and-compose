201ded41ecaaa7a888726769e96b23b69ffdbe70
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2023-08-15 22:54:36 +0300

    1

--- frontend/ormconfig.ts
+++
@@ -1,9 +0,0 @@
-import { DataSource } from 'typeorm';
-import { config } from 'dotenv';
-
-config();
-
-import configuration from './src/configuration/configuration';
-
-const options = configuration().database;
-export const AppDataSource = new DataSource(options);
